af10dc7221d3277c64fd835fa1375c64d5dc8f81
M_chowdhury <59116892+SgtClutch@users.noreply.github.com>
2024-01-25 19:06:28 -0500

    Create database.pp

---
+++ site-modules/profile/manifests/database.pp
@@ -0,0 +1,48 @@
+class profile::postgresql (
+  $postgres_password = 'TPSrep0rt!',
+) {
+  package { 'postgresql-server':
+    ensure => installed,
+  }
+
+  package { 'postgresql-contrib':
+    ensure => installed,
+  }
+
+  package { 'postgresql-client':
+    ensure => installed,
+  }
+
+  exec { 'initdb':
+    command     => '/usr/bin/postgresql-setup initdb',
+    refreshonly => true,
+  }
+
+  service { 'postgresql':
+    ensure  => running,
+    enable  => true,
+    require => [Package['postgresql-server'], Exec['initdb']],
+  }
+
+  file { '/var/lib/pgsql/data/pg_hba.conf':
+    ensure  => file,
+    content => "host    all             all             0.0.0.0/0            md5\n",
+    require => Service['postgresql'],
+  }
+
+  file { '/var/lib/pgsql/data/postgresql.conf':
+    ensure  => file,
+    content => "listen_addresses = '*'\n",
+    require => Service['postgresql'],
+  }
+
+  exec { 'set_postgres_password':
+    command => "/usr/bin/psql -U postgres -c \"ALTER USER postgres WITH PASSWORD '${postgres_password}';\"",
+    unless  => "/usr/bin/psql -U postgres -c 'SELECT 1' | grep -q 1",
+    require => Service['postgresql'],
+  }
+
+  package { 'postgresql-client':
+    ensure => installed,
+  }
+}
