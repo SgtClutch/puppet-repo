a6b1101549316fee997de78580f273c467931bdd
M_chowdhury <59116892+SgtClutch@users.noreply.github.com>
2024-02-01 13:00:44 -0500

    Update postgresql.pp

--- site-modules/profile/manifests/postgresql.pp
+++ site-modules/profile/manifests/postgresql.pp
@@ -30,7 +30,7 @@ class profile::postgresql  {
 
   file_line { 'allow_all_users_ipv4':
     path    => '/etc/postgresql/14/main/pg_hba.conf',
-    line    => 'host    all             all             0.0.0.0/0            md5',
+    line    => 'host    all             all             0.0.0.0/0               md5',
     match   => '^host\s+all\s+all\s+127.0.0.1/32\s+\sscram-sha-256$',
     notify  => Service['postgresql'],
     
@@ -39,7 +39,7 @@ class profile::postgresql  {
 
   file_line { 'all_listen_address':
     path    => '/etc/postgresql/14/main/postgresql.conf',
-    line    => 'listen_address=*',
+    line    => "listen_address= '*'",
     match   => "^#listen_addresses = 'localhost'",
     notify  => Service['postgresql'],
     
